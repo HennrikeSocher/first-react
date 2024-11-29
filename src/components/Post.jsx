cd40058ad612cb3e9e314cd288a973e0ee4726af
HennrikeSocher <socherhennrike@gmail.com>
2024-11-29 18:44:15 -0300

    new styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,8 +2,31 @@ import styles from "./Post.module.css"
 
 export function Post() {
   return (
-    <article className={styles.article}>
-      <p>Hello World</p>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/hennrikesocher.png" />
+          <div className={styles.authorInfo}>
+            <strong>Diego Fernandes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time dateTime="2024-05-11 00:02:13">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
     </article>
   )
 }
